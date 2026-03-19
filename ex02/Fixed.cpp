985001fe7266c66721441eead5bad7c024ac5003
Said Belabbas <saibelab@f6r3s7.clusters.42paris.fr>
2026-03-19 16:21:39 +0100

    finish

---
+++ ex02/Fixed.cpp
@@ -0,0 +1,100 @@
+#include "Fixed.hpp"
+#include <iostream>
+#include <cmath>
+
+// ======================= CONSTRUCTEURS =======================
+Fixed::Fixed() : _rawbits(0) {
+}
+
+Fixed::Fixed(const int n) : _rawbits(n << _storage) {
+}
+
+Fixed::Fixed(const float f) : _rawbits(roundf(f * (1 << _storage))) {
+}
+
+Fixed::Fixed(const Fixed& other) : _rawbits(other._rawbits) {
+}
+
+Fixed& Fixed::operator=(const Fixed& ref) {
+	if (this != &ref)
+		_rawbits = ref._rawbits;
+	return *this;
+}
+
+Fixed::~Fixed() {
+}
+
+// ======================= GETTER / SETTER =======================
+int Fixed::getRawBits(void) const {
+	return _rawbits;
+}
+
+void Fixed::setRawBits(int const raw) {
+	_rawbits = raw;
+}
+
+// ======================= CONVERSION =======================
+int Fixed::toInt(void) const {
+	return _rawbits >> _storage;
+}
+
+float Fixed::toFloat(void) const {
+	return (float)_rawbits / (1 << _storage);
+}
+
+// ======================= OPERATOR << =======================
+std::ostream& operator<<(std::ostream& flux, const Fixed& obj) {
+	flux << obj.toFloat();
+	return flux;
+}
+
+// ======================= OPÉRATEURS ARITHMÉTIQUES =======================
+Fixed Fixed::operator+(const Fixed& ref) const {
+	Fixed temp;
+	temp.setRawBits(_rawbits + ref.getRawBits());
+	return temp;
+}
+
+Fixed Fixed::operator-(const Fixed& ref) const {
+	Fixed temp;
+	temp.setRawBits(_rawbits - ref.getRawBits());
+	return temp;
+}
+
+Fixed Fixed::operator*(const Fixed& ref) const {
+	Fixed temp;
+	temp.setRawBits((_rawbits * ref.getRawBits()) >> _storage);
+	return temp;
+}
+
+Fixed Fixed::operator/(const Fixed& ref) const {
+	Fixed temp;
+	temp.setRawBits((_rawbits << _storage) / ref.getRawBits());
+	return temp;
+}
+
+// Pré-incrément
+Fixed& Fixed::operator++() {
+	_rawbits += 1;
+	return *this;
+}
+
+// Post-incrément
+Fixed Fixed::operator++(int) {
+	Fixed temp(*this);
+	_rawbits += 1;
+	return temp;
+}
+
+// Pré-décrément
+Fixed& Fixed::operator--() {
+	_rawbits -= 1;
+	return *this;
+}
+
+// Post-décrément
+Fixed Fixed::operator--(int) {
+	Fixed temp(*this);
+	_rawbits -= 1;
+	return temp;
+}
---
+++ ex02/Fixed.hpp
@@ -0,0 +1,42 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+	public:
+		Fixed();
+		Fixed(const int n);
+		Fixed(const float f);
+		Fixed(const Fixed& other);
+		Fixed& operator=(const Fixed& other);
+		~Fixed();
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+		float toFloat(void) const;
+		int toInt(void) const;
+		bool operator>(const Fixed& obj) const;
+		bool operator>=(const Fixed& obj) const;
+		bool operator<=(const Fixed& obj) const;
+		bool operator<(const Fixed& obj) const;
+		bool operator==(const Fixed& obj) const;
+		bool operator!=(const Fixed& obj) const;
+		Fixed operator+(const Fixed& obj) const;
+		Fixed operator*(const Fixed& obj) const;
+		Fixed operator-(const Fixed& obj) const;
+		Fixed operator/(const Fixed& obj) const;
+		Fixed operator++(int);
+		Fixed& operator++();
+		Fixed operator--(int);
+		Fixed& operator--();
+
+		private:
+		int _rawbits;
+		static const int _storage = 8;
+	};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& obj);
+
+#endif
